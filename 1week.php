b91840b3ca2e5b07c8e06d78c5f47f335beb8488
erzanni <38134363+erzanni@users.noreply.github.com>
2018-11-13 20:23:04 +0530

    Add files via upload

---
+++ 1week.php
@@ -0,0 +1,73 @@
+<html>
+
+<head>
+
+<?php
+	include("includes1/week.html");
+ ?>
+
+</head>
+<body>
+
+<?php
+
+ include("includes1/menu.html");
+ ?>
+ <div id="box">
+ <div id="container">
+<div id="week_content">
+
+					<h2 align="center">FIRST WEEK PREGNANT</h2>
+					<img src="../week_images/w1.png" alt="Img">
+					<p>
+					Surprise! One week pregnant is actually not pregnant at all. Say what?!
+
+					Most OBs count pregnancy starting from the first day of your last period. That's because ovulation and conception are really tough to pinpoint, but the physical signs of menstruation are much easier to spot. So in doctor speak, 1 week pregnant means you’re on your period and not yet pregnant. You won’t actually conceive your baby until week two or three, depending on the length of your cycle.
+
+					If you’ve just found out you’re expecting, you might think this is your first week of pregnancy, but you’re probably around 4 weeks pregnant. Yes, already! (Your OB will give you a more accurate estimate when you go in for your first prenatal appointment.) Skip ahead and check out week 4.
+					</p>
+					<h2>FIRST WEEK PREGNANT SYMPTOMS</h2>
+					<p>
+					At 1 week pregnant—remember at this early stage, the symptoms you're experiencing are those typical to your period because you're not actually pregnant. These symptoms may last from three to seven days, and can include:<br/>
+					Bloating. Fluctuating hormones can give you a bloated belly right before and during your period.
+					Mood swings. Those raging hormones can also cause irritability and wreak havoc on your emotions.<br/>
+					Headache. Many women complain of menstrual migraines, which are also hormone related. (Surprise, surprise.) Ice packs, OTC pain relievers, and relaxation exercises may help ease the pain.<br/>
+					</p>
+					<h2>FIRST WEEK PREGNANT BELLY</h2>
+					<p>
+					Inside your 1 week pregnant belly, your body isn’t only releasing last month’s egg. It begins to form a new uterine lining, which will hold next month’s egg. You know, the one that will hopefully get fertilized and grow and develop into a fetus?!<br/>
+					
+					Cool fact: Human eggs are the largest cells in the body. An egg is the size of peach fuzz. Your body will release one (or in rare cases, two) eggs somewhere between day 10 and day 19 of your menstrual cycle—or about 14 days before your next period is expected. It can be fertilized for about 12 to 24 hours after that. Oh, and having sex before then is helpful too, since sperm can live in your body for up to six days.<br/>
+					The first signs of pregnancy won’t occur right away—in fact, many women miss their period at week 4 before they even feel “different.” But some common early signs of pregnancy in the first weeks after fertilization include breast soreness or tenderness, nausea, fatigue, and frequent urge to pee.<br/>
+					
+					At one week pregnant (a.k.a. hoping to be pregnant soon!), you’re probably thinking about how you can prep your body to carry a baby. It’s a good idea to start pregnancy at a healthy weight and free of bad habits such as smoking and drinking. Reduce your caffeine intake to the equivalent of a cup of coffee or two (we’re talking 8 ounce cups—not the venti!) per day.<br/>
+					</p>
+					<h2> FIRST WEEK PREGNANT ULTRASOUND</h2>
+					<p>
+					You’re not likely to have a 1 week pregnant ultrasound. But if you’ve been trying for a while to get pregnant and have seen a fertility specialist, you may have had an ultrasound to check for fibroids, to see how many follicles (which grow into eggs) are in your ovaries and/or to gauge the thickness of your uterine lining. If there are any problems, your doctor can prescribe a fertility treatment that can help you conceive.<br/>
+					</p>
+					<h2>
+					PREGNANCY CHECKLIST AT FIRST WEEK PREGNANT
+					</h2>
+					<p
+					Reminders for the week:<br/>
+					
+					Stop unhealthy habits such as smoking, drinking, and excessive caffeine<br/>
+
+					Take a prenatal vitamin with 400 mcg folic acid daily<br/>
+					Track your cycle to pinpoint your most fertile days<br/>
+					
+					</p>
+	
+</div>
+</div>
+</div>
+
+<footer>
+<?php
+
+ include("includes1/footer.html");
+ ?>
+ </footer>
+ </body>
+ </html>
